92a1283f7091f5a735065bc6734de15eb9b06316
Carlos Caicedo <calicheccmusica@gmail.com>
2022-02-11 14:06:53 -0800

    initial commit

---
+++ src/main/java/com/carloscaicedo/daikichiroutes/DaikichiController.java
@@ -0,0 +1,36 @@
+package com.carloscaicedo.daikichiroutes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController	
+@RequestMapping("/greeting")
+public class DaikichiController {
+	
+	@RequestMapping("")
+	public String welcome_page() {
+		return "Welcome to Daikichi. Wonderful thinkgs are about to come for you";
+	}
+	
+	
+	@RequestMapping("/today")
+	public String today_page() {
+		return "Today is a great day to catch up on assigments and perhaps even to get ahead!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow_page() {
+		return "Tomorrow will bring some deserve time with family, "
+				+ "as well as some rest. And Tomorrow you'll have some time to code as well.!";
+	}
+	
+	@GetMapping("/days_later")
+	public String week_later_page() {
+		return "In a few days you'll start a new live. Enjoy!";
+	}
+	
+
+}
+
+
---
+++ src/main/java/com/carloscaicedo/daikichiroutes/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.carloscaicedo.daikichiroutes;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DaikichiroutesApplication.class);
+	}
+
+}
